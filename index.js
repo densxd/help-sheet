4c4b472ee3fdb0afe4539118d1f92ae0c8190625
densxd <82174228+densxd@users.noreply.github.com>
2022-01-09 19:35:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let result;
 /**
  * @param { Array } array
 */
+
+// [ [ ["data"] ] ] Üçlü arraye kadar destekliyor.
 const Find = (array, key) => {
     if (!Array.isArray(array)) console.log(new Error(`Belirttiğin obje array değil.`))
     for (let i = 0; i < array.length; i++) {
@@ -20,6 +22,7 @@ const Find = (array, key) => {
 /**
  * @param { Object[]} Obj
  */
+// [ [ {"ID": "1},{"NAME":"Dens"} ] ] İkili arreye kadar destekliyor.
 const ObjFind = (Obj, key) => {
     if (Object.prototype.toString.call(Obj) !== "[object Array]" && Obj.length == undefined) return console.log(new Error(`Belirttiğin obje [object Array] değil.`));
     if (!Obj || !key) return console.log(new Error("Arrayı ve ya Keyini belirtmen gerek."))
@@ -34,6 +37,8 @@ const ObjFind = (Obj, key) => {
  * @param { Object{} } data
  * @param { Array[] } variable
  */
+
+// Databasedeki arrayin içine kolayca istediğin şeyi pushlama.
 const EzPush = async (data, variable, item) => {
     if (!data || !variable || !item) return console.log(new Error("Argümanları doğru giriniz. Örn = KolayPush(data, Isimler, pushlanacak şey)"))
     try {
@@ -45,6 +50,9 @@ const EzPush = async (data, variable, item) => {
  * @param { Object{} } data
  * @param { String } variable
  */
+
+// Databasedeki arrayin içine kolayca istediğin şeyi pullama.
+
 const EzPull = async (data, variable, item) => {
     if (!data || !variable || !item) return console.log(new Error("Argümanları doğru giriniz. Örn = KolayPull(data, Isimler, çıkartılacak şey)"))
     try {
@@ -55,20 +63,25 @@ const EzPull = async (data, variable, item) => {
 /**
  * @param { Array[] } array 
  */
+
+// İstediğin arrayi karıştırma.
 const Shuffle = (array) => {
     return array[Math.floor((Math.random() * array.length))];
 }
 
+// Promise ile sleep.
 const Sleep = (ms) => {
     return new Promise((resolve) => {
         setTimeout(resolve, ms);
     });
 }
 
+// İstediğiniz kadar random sayı oluşturma.
 const RandomNumber = (min, max) => {
     return Math.round(Math.random() * (max - min) + min);
 }
 
+// İstediğiniz kadar random karater oluşturma.
 const RandomChar = (max, caps) => {
     let chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     if(caps){chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];}
@@ -79,6 +92,7 @@ const RandomChar = (max, caps) => {
     return value;
 }
 
+// İnvite/Link regex kontrolcü boolean cevap verir.
 const CheckReg = (content,option) => {
     let inviteRegex = /(https:\/\/)?(www\.)?(discord\.gg|discord\.me|discordapp\.com\/invite|discord\.com\/invite)\/([a-z0-9-.]+)?/i;  
     let linkRegex = /(http[s]?:\/\/)(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)/gi;
