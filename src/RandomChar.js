5268b53a94dc5e2f89a19d3d1bbce00c53f314ab
densxd <82174228+densxd@users.noreply.github.com>
2022-01-28 15:04:56 +0300

    Update RandomChar.js

--- src/RandomChar.js
+++ src/RandomChar.js
@@ -2,6 +2,7 @@
 
 // Random karakter'de Caps özelliğini kullanmak için parametrelerde true/false geçirebilirsiniz. 
 // Boş ise küçük verir.
+const Shuffle = require('./Shuffle');
 module.exports = function RandomChar(max, caps) {
     let chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     if (caps) { chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; }
